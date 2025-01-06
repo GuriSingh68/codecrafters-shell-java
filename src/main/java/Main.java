e9c2b26ffec7950a754c2fa33451eb8977be82d5
GuriSingh68 <141493117+GuriSingh68@users.noreply.github.com>
2025-01-05 20:20:04 -0500

    exit button

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,15 +4,11 @@ public class Main {
     public static void main(String[] args) throws Exception {
 
         Scanner scanner = new Scanner(System.in);
-        boolean ip= true;
-        while(ip){
+        while(true){
             System.out.print("$ ");
             String input = scanner.nextLine();
             System.out.println(input+": command not found");
-            ip=false;
-        }
-        if(!ip){
-            System.out.println("$ "+"exit 0");
+            System.exit(0);
         }
     }
 }
