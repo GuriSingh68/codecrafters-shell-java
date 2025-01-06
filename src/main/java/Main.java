7bc4bdcea3d41942150a2363040ddf5029894a4f
GuriSingh68 <141493117+GuriSingh68@users.noreply.github.com>
2025-01-06 03:33:17 -0500

    exit button

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,12 +4,10 @@ public class Main {
     public static void main(String[] args) throws Exception {
 
         Scanner scanner = new Scanner(System.in);
-        while(true){
+        
             System.out.print("$ ");
             String input = scanner.nextLine();
             System.out.println(input+": command not found");
-            System.out.println("$");
             System.exit(0);
-        }
     }
 }
