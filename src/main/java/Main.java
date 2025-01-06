4fc3dca04ad892132651d2cc07a765e7b0dd5686
GuriSingh68 <141493117+GuriSingh68@users.noreply.github.com>
2025-01-06 14:28:28 -0500

    built in commands

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,25 +4,26 @@ public class Main {
     public static void main(String[] args) throws Exception {
 
         Scanner scanner = new Scanner(System.in);
-        while(true){
+        while (true) {
             System.out.print("$ ");
             String input = scanner.nextLine();
-            if(input.equals("exit 0")){
-               break;
+            if (input.equals("exit 0")) {
+                break;
             }
-            if(input.startsWith("type")){
-                input=input.substring(4);
-                if(input.equals("echo")) 
-                    System.out.println("echo is a shell builtin");
-                else if(input.equals("exit"))
-                    System.out.println("exit is a shell builtin");
+            if (input.startsWith("type")) {
+               String[] command=input.split("\\s+");
+                for(String str:command){
+                    if(str.equals("exit"))
+                        System.out.println("exit is a shell builtin");
+                    else if(str.equals("echo"))
+                        System.out.println("echo is a shell builtin");
+                }
             }
-             if(input.startsWith("echo")){
-                input=input.substring(5);
+           else if (input.startsWith("echo")) {
+                input = input.substring(5);
                 System.out.println(input);
-            }
-            else{
-                System.out.println(input+": command not found");
+            } else {
+                System.out.println(input + ": command not found");
             }
         }
     }
